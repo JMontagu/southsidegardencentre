0b7a2e769022512f8e324dcbb90f68a35c4975ce
Jade Montagu <jade@jademontagu.com>
2019-04-28 20:24:56 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,10 @@
-default_cache = 3600
+#!/bin/bash
 
+default_cache = 3600
+      
 # Index.html page
 aws s3 sync dist/ s3://www.southsidegardencentre.co.nz \
     --exclude "*.jpg" --exclude "*.png" --exclude "pages/*" --exclude "*.html" --exclude "*.css" --include "index.html" \
-    --content-language "en-NZ" \
-    --metadata-directive REPLACE --cache-control max-age=$default_cache \
-    --acl public-read \
-    --region ap-southeast-2 --delete
-
-# All other 'extensionless' html files
-aws s3 sync dist/pages s3://www.southsidegardencentre.co.nz \
-    --content-type text/html \
     --content-language "en-NZ" \
     --metadata-directive REPLACE --cache-control max-age=$default_cache \
     --acl public-read \
@@ -20,5 +14,13 @@ aws s3 sync dist/pages s3://www.southsidegardencentre.co.nz \
 aws s3 sync dist/ s3://www.southsidegardencentre.co.nz \
     --exclude "*" --include "*.jpg" --include "*.png" --include "*.css" \
     --metadata-directive REPLACE --expires 2034-01-01T00:00:00Z --cache-control max-age=2592000 \
+    --acl public-read \
+    --region ap-southeast-2 --delete
+
+# All other 'extensionless' html files
+aws s3 sync dist/pages s3://www.southsidegardencentre.co.nz \
+    --content-type text/html \
+    --content-language "en-NZ" \
+    --metadata-directive REPLACE --cache-control max-age=$default_cache \
     --acl public-read \
     --region ap-southeast-2 --delete
